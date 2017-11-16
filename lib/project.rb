2b46c2a9f4a94e6eb471a6f450c1b2b6a3e64de7
Chris Cardle <cdcardle@gmail.com>
2017-11-16 15:05:45 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,16 @@
 class Project
   attr_accessor :title
+  @@backers
 
   def initialize(title)
     @title = title
+  end
+
+  def self.backers
+    @@backers
+  end
+
+  def add_backer(backer)
+    self.backer << backer
   end
 end
